4402c450d8b3914469597d3607cfd5d4cf04e7e8
Brandon Goldman <brandon.goldman@gmail.com>
2015-02-01 22:01:59 -0800

    autoload for users without composer

---
+++ autoload.php
@@ -0,0 +1,47 @@
+<?php
+if (!class_exists('GuzzleHttp\Client')) {
+    throw new Exception('The Box View SDK requires GuzzleHttp\Client.');
+}
+
+/**
+ * You only need this file if you are not using composer.
+ * Why are you not using composer?
+ * https://getcomposer.org/
+ */
+
+/**
+ * Register the autoloader for the Box View SDK classes.
+ * Based off the official PSR-4 autoloader example found here:
+ * https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-4-autoloader-examples.md
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = 'Box\\View\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/src/Box/View/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
